4160a48ff0531ec47f95a8954c77496097cc3f04
Noah <noah.t.chen@gmail.com>
2024-02-23 23:16:13 -0500

    Solved ccc23s4

--- CCC/2023/ccc23s4.py
+++ CCC/2023/ccc23s4.py
@@ -4,36 +4,42 @@ import sys, math, heapq as hq, itertools, copy
 input, inf, mx, mod = sys.stdin.readline, 1e30, 302, 1000000007
 # sys.setrecursionlimit(100000)
 
-def find(n):
-    if n != ds[n]:
-        ds[n] = find(ds[n])
-    return ds[n]
-
-def merge(a, b):
-    a = find(a)
-    b = find(b)
-    if a == b:
-        return
-    else:
-        ds[a] = b
+def dijkstra(a, b):
+    dis = [inf] * (N+1)
+    dis[a] = 0
+    q = [[0, a]]
+    while q:
+        d, u = hq.heappop(q)
+        if u == b:
+            break
+        if d != dis[u]:
+            continue
+        for l, v in adjl[u]:
+            if dis[u] + l < dis[v]:
+                dis[v] = dis[u] + l
+                hq.heappush(q, [dis[v], v])
+    return dis[b]
 
 N, M = map(int, input().split())
 edges = []
 ds = [i for i in range(N+1)]
+adjl = [[] for _ in range(N+1)]
 
 for _ in range(M):
     u, v, l, c = map(int, input().split())
-    edges.append([c, u, v])
-
+    edges.append([l, c, u, v])
+    
 edges.sort()
-# kruskal
 
 ans = 0
-for d, a, b in edges:
-    fa = find(a)
-    fb = find(b)
-    if fa != fb:
-        ds[fa] = fb
-        ans += d
-
+for l, c, a, b in edges:
+    d1 = dijkstra(a, b)
+    adjl[a].append([l, b])
+    adjl[b].append([l, a])
+    d2 = dijkstra(a, b)
+    if d2 < d1:
+        ans += c
+    else:
+        adjl[a].pop()
+        adjl[b].pop()
 print(ans)
